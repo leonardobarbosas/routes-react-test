df6a79854375fcda5299b8291779a3e8890814a2
leonardobarbosas <leonardobarbosasantos@gmail.com>
2024-08-21 11:31:32 -0300

    adicionando componentes de rotas

--- aula06/src/main.tsx
+++ aula06/src/main.tsx
@@ -1,9 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./routes/Home/index.tsx";
+import Produtos from "./routes/Produtos/index.tsx";
+import EditarProdutos from "./routes/EditarProdutos/index.tsx";
+import Error from "./routes/Error/index.tsx";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/produtos",
+        element: <Produtos />,
+      },
+      {
+        path: "/editarProdutos",
+        element: <EditarProdutos />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
